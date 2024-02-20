b967c5add23e9bb543d25be7dc739604a3d2c4cc
no215tyler <no.215.tyler@gmail.com>
2024-02-20 17:28:07 +0900

    タスク3.Step2（カーソルの動きに連動して記事に影をつける）

--- app/javascript/article.js
+++ app/javascript/article.js
@@ -1,5 +1,18 @@
 const focus = () => {
-  console.log("do reload");
+  const articles = document.querySelectorAll(".article");
+
+  articles.forEach(function (article) {
+    article.addEventListener("mouseover", function () {
+      this.setAttribute(
+        "style",
+        "box-shadow: 10px 10px 10px 0 rgba(0, 0, 0, 0.5)"
+      );
+    });
+
+    article.addEventListener("mouseout", function () {
+      this.removeAttribute("style");
+    });
+  });
 };
 
 window.addEventListener("turbo:load", focus);
