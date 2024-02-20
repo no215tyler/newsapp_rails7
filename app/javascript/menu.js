960393f9da389ed3b7b3220d051ed40a4b695825
no215tyler <no.215.tyler@gmail.com>
2024-02-20 11:05:03 +0900

    タスク2.Step2（カーソルの動きに連動してメニュー配色を変更）

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -1,5 +1,14 @@
 const menu = () => {
   console.log("リロードで発火");
+  const pullDownButton = document.getElementById("menu1");
+
+  pullDownButton.addEventListener("mouseover", function () {
+    this.setAttribute("style", "background-color: #3136bd;");
+  });
+
+  pullDownButton.addEventListener("mouseout", function () {
+    this.removeAttribute("style");
+  });
 };
 
 window.addEventListener("turbo:load", menu);
